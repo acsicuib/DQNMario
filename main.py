c41b0949203f3f0ff9b4c301e387df9b3d6ab894
IsaacLera <isaac.lera@gmail.com>
2022-03-08 13:00:26 +0100

    updates

--- main.py
+++ main.py
@@ -109,8 +109,8 @@ for episode in range(1,episodes+1):
     fps = (frame - start_frame) / (end_time - start_time)
     moves = step
     # To comment
-    if moves <10:
-        print('episode: {}, frame: {}, moves: {}, score: {}'.format(episode, frame, int(moves), score))
+
+    print('episode: {}, frame: {}, moves: {}, score: {}'.format(episode, frame, int(moves), score))
 
     writer.add_scalar('fps', fps, episode)
     writer.add_scalar('score/frame', score, episode)
